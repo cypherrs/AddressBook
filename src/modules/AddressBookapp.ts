fd69f0b766f14b7e3c0d7c69d26dbb9976d4d17b
Rupesh R Sahoo <rs0722@srmist.edu.in>
2025-06-27 11:34:55 +0530

    added Uc3 editing the contact as select

--- src/modules/AddressBookapp.ts
+++ src/modules/AddressBookapp.ts
@@ -1,7 +1,5 @@
-//uc1 : contact.ts
-import readline from "readline";
-
-class Contact{
+// UC1: Creating Contact class
+class Contact {
     public firstName: string;
     public lastName: string;
     public address: string;
@@ -25,57 +23,72 @@ class Contact{
     displayContact(): void {
         console.log("-----  Contact Details  -----");
         console.log(`Name         : ${this.firstName} ${this.lastName}`);
-        console.log(`Address      : ${this.address}, ${this.city}, ${this.state}`);
+        console.log(`Address      : ${this.address}, ${this.city}, ${this.state} - ${this.zip}`); // zip added to display
         console.log(`Phone Number : ${this.phoneNumber}`);
         console.log(`Email        : ${this.email}`);
     }
 }
 
-// const contact1 = new Contact("Rupesh","Roshan","Q-123","Angul","Odisha","795145","8658684467","rupeshroshansahoo@gmail.com");
-//
-// contact1.displayContact();
+import * as readline from 'readline';
 
-//uc2 : AddressBook class to manage contact list
-
+// UC2: Creating AddressBook class
 class AddressBook {
 
     private contacts: Contact[] = [];
 
-    addContact(contact: Contact){
+    addContact(contact: Contact) {
         this.contacts.push(contact);
-        console.log('the Constact added Successfuly \n');
+        console.log('the Contact added Successfully \n'); // fixed spelling: Constact → Contact
     }
 
-    displayAllContact(){
+    displayAllContact() {
         console.log('-----  All Contact  -----');
-        this.contacts.forEach((contact , index) => {
+        this.contacts.forEach((contact, index) => {
             console.log(` Contact ${index + 1}`);
             contact.displayContact();
-        })
+        });
+    }
+
+    // UC3: finding the contact to edit using first name and updating field
+    editContact(firstName: string, updateField: string, newValue: string): boolean {
+        const contact = this.contacts.find(c => c.firstName.toLowerCase() === firstName.toLowerCase());
+        if (contact) {
+            if (updateField in contact) {
+                (contact as any)[updateField] = newValue; // dynamic update of property
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // UC3: Getter to safely access contacts from AddressBookMain
+    getContacts(): Contact[] {
+        return this.contacts;
     }
 }
 
-class AddressBookMain{
-    private addressBook : AddressBook = new AddressBook();
-    private r1 = readline .createInterface({
-        input : process.stdin,
-        output : process.stdout
+// UC2: Using Console to add person details from AddressBookMain class
+class AddressBookMain {
+    private addressBook: AddressBook = new AddressBook();
+    private r1 = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
 
-    private addContactFlow():void{
-        const contactData :any = {};
+    private addContactFlow(): void {
+        const contactData: any = {};
 
-        this.r1.question("First name: ", (firstName)=>{
+        this.r1.question("First name: ", (firstName) => {
             contactData.firstName = firstName;
 
-            this.r1.question("Last name: ", (lastName) =>{
+            this.r1.question("Last name: ", (lastName) => {
                 contactData.lastName = lastName;
 
-                this.r1.question("Address: ", (address)=> {
+                this.r1.question("Address: ", (address) => {
                     contactData.address = address;
 
-                    this.r1.question("City: ", (city)=>{
-                        contactData.city =city;
+                    this.r1.question("City: ", (city) => {
+                        contactData.city = city;
 
                         this.r1.question("State: ", (state) => {
                             contactData.state = state;
@@ -88,7 +101,6 @@ class AddressBookMain{
 
                                     this.r1.question("Email: ", (email) => {
                                         contactData.email = email;
-
 
                                         const newContact = new Contact(
                                             contactData.firstName,
@@ -115,21 +127,54 @@ class AddressBookMain{
         });
     }
 
-    start() : void{
-        console.log("Welcome To Address Book!");
-        this.r1.question("Do u want to add an new contact? (y/n) : " ,(answer) =>{
-            if ( answer.toLowerCase() === "yes"|| "y"){
-                this.addContactFlow();
-            } else {
-                console.log("exiting Address book");
+    // UC3: Updating the edit control flow using Console
+    private editContactFlow(): void {
+        this.r1.question("Enter the first name of the contact to edit: ", (firstName) => {
+            const contact = this.addressBook.getContacts().find(c => c.firstName.toLowerCase() === firstName.toLowerCase()); // using getter
+
+            if (!contact) {
+                console.log("Contact not found.");
                 this.r1.close();
+                return;
             }
 
+            this.r1.question("Which field do you want to edit? (lastName, address, city, state, zip, phoneNumber, email): ", (field) => {
+                if (!(field in contact)) {
+                    console.log("Invalid field.");
+                    this.r1.close();
+                    return;
+                }
+
+                this.r1.question(`Enter new value for ${field}: `, (newValue) => {
+                    const success = this.addressBook.editContact(firstName, field, newValue);
+                    if (success) {
+                        console.log("Contact updated successfully.");
+                        this.addressBook.displayAllContact();
+                    } else {
+                        console.log("Update failed.");
+                    }
+                    this.r1.close();
+                });
+            });
         });
+    }
 
-
+    // UC2 & UC3: Updating start() to choose between add or edit
+    start(): void {
+        console.log("Welcome To Address Book!");
+        this.r1.question("Choose an option: \n1. Add new contact\n2. Edit contact\nEnter 1 or 2: ", (option) => {
+            if (option === "1") {
+                this.addContactFlow();
+            } else if (option === "2") {
+                this.editContactFlow();
+            } else {
+                console.log("Invalid choice. Exiting...");
+                this.r1.close();
+            }
+        });
     }
 }
 
+// UC2: Running the main class
 const addressBook1 = new AddressBookMain();
 addressBook1.start();
